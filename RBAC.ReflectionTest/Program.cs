2e766043c4c30778036d7877fcd76d8a08a8b3e9
xiao <superfeeling@126.com>
2021-08-23 12:06:07 +0800

    反射基础

---
+++ RBAC.ReflectionTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace RBAC.ReflectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Student);
+
+            foreach (var item in type.GetProperties())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
